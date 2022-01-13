091c0590f9c3bb845e96b309610eadedc25347b6
theart0 <81599811+theart0@users.noreply.github.com>
2022-01-14 03:56:30 +0700

    login with no data

--- src/test/java/stepdefinitions/login_no_data.java
+++ src/test/java/stepdefinitions/login_no_data.java
@@ -6,8 +6,12 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import utils.helpers.Helpers;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class login_no_data extends  BaseStepDef {
 
@@ -25,21 +29,19 @@ public class login_no_data extends  BaseStepDef {
         Helpers.delay(1);
     }
 
-    @When("user click button login but no input data user and password")
-    public void userClickButtonLoginButNoInputDataUserAndPassword() {
+    @When("user click button login and click login")
+    public void userclickbuttonlogin() {
         Helpers.delay(1);
-        driver.findElement(By.xpath("//a[@id='Lnew1']")).click();
-
+        driver.findElement(By.xpath("//a[@id='login2']")).click();
+        driver.findElement(By.xpath("//button[normalize-space()='Log in']")).click();
     }
+    
 
-
-    @Then("user can't login in the next display")
-    public void userCanTLoginInTheNextDisplay() {
-        Assert.assertEquals(driver.getCurrentUrl(), "https://my.uda.edu.vn/sv/svlogin");
+    @Then("a notice login fail appear")
+    public void aNoticeLoginFailAppear() {
+        Helpers.delay(3);
+        String thongbao =  driver.switchTo().alert().getText();
+        Assert.assertEquals(thongbao,"Please fill out Username and Password.","Login Succesful");
     }
-
-
-
-
 }
 
--- src/test/resources/features/login_no_data.feature
+++ src/test/resources/features/login_no_data.feature
@@ -2,8 +2,8 @@ Feature: login with no data
 
   Scenario Outline:  user login with no data user and password
     Given user navigate to "<url>"
-    When user click button login but no input data user and password
-    Then user can't login in the next display
+    When user click button login and click login
+    Then a notice login fail appear
     Examples:
-      | url                              |
-      | https://my.uda.edu.vn/sv/svlogin |
+      | url                                  |
+      | https://www.demoblaze.com/index.html |
